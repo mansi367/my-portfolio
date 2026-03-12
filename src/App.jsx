2cd922a8eecd55536e56df113598a7e0fe84f743
mansi <mansibhargav765@gmail.com>
2026-03-12 19:33:54 +0530

    Fix Contact import case

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,18 @@
-
 import './App.css'
 import Nav from './components/Nav/Nav'
 import Home from './components/Home/Home'
 import About from './components/About/About'
 import Projects from './components/Projects/Projects'
-import Contact from './components/Contact/contact'
+import Contact from './components/Contact/Contact'  // <-- Capital C
 
 function App() {
-  
-
   return (
     <>
-    <Nav/>
-    <Home/>
-    <About/>
-    <Projects/>
-    <Contact/>
-  
+      <Nav/>
+      <Home/>
+      <About/>
+      <Projects/>
+      <Contact/>
     </>
   )
 }
