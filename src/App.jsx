b830c4738de27d13be137b22296141c56664871f
mansi <mansibhargav765@gmail.com>
2026-03-12 19:35:24 +0530

    Make imports Vercel-safe

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,16 @@ import Nav from './components/Nav/Nav'
 import Home from './components/Home/Home'
 import About from './components/About/About'
 import Projects from './components/Projects/Projects'
-import Contact from './components/Contact/Contact'  // <-- Capital C
+import Contact from './components/Contact/Contact' // Capital C
 
 function App() {
   return (
     <>
-      <Nav/>
-      <Home/>
-      <About/>
-      <Projects/>
-      <Contact/>
+      <Nav />
+      <Home />
+      <About />
+      <Projects />
+      <Contact />
     </>
   )
 }
